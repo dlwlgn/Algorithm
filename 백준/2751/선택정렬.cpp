d17c363092dbbcb74aa772dbb853a3f740c7e9d8
dlwlgn <50227342+dlwlgn@users.noreply.github.com>
2021-06-22 21:09:53 +0900

    Update 선택정렬.cpp

--- 백준/2751/선택정렬.cpp
+++ 백준/2751/선택정렬.cpp
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
-int arr[1000001];
+int N, arr[1000001];
 
-int main(){
-    int N;
-    scanf("%d",&N);
-    for(int i=0; i<N; i++){
-        scanf("%d",&arr[i]);
-    }
-    
-    for(int i=0; i<N-1; i++){
-        int min = 1000001, min_arr;
-        for(int j=i; j<N; j++){
-            if(min > arr[j]){
-                min = arr[j];
-                min_arr = j;
-            }
-        }
-        int temp = arr[i];
-        arr[i] = arr[min_arr];
-        arr[min_arr] = temp;
-    }
-    
-    for(int i=0; i<N; i++){
-        printf("%d\n",arr[i]);
-    }
+void selection() {
+	for (int i = 0; i < N - 1; i++) {
+		int min = 1000001, min_arr;
+		for (int j = i; j < N; j++) {
+			if (min > arr[j]) {
+				min = arr[j];
+				min_arr = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = arr[min_arr];
+		arr[min_arr] = temp;
+	}
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	selection();
+
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", arr[i]);
+	}
 }
