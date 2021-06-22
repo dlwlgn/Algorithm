c6a70c3fff78d527cccbcb158f388cbabe0acd79
dlwlgn <50227342+dlwlgn@users.noreply.github.com>
2021-06-22 21:06:49 +0900

    Update 버블정렬.cpp

--- 백준/2751/버블정렬.cpp
+++ 백준/2751/버블정렬.cpp
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
-int arr[1000001];
+int N, arr[1000001];
 
-int main(){
-    int N;
-    scanf("%d",&N);
-    for(int i=0; i<N; i++){
-        scanf("%d",&arr[i]);
-    }
-    
-    for(int i=0; i<N; i++){
-        for(int j=0; j<N-1-i; j++){
-            if(arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
-        }
-    }
-    
-    for(int i=0; i<N; i++){
-        printf("%d\n",arr[i]);
-    }
+void bubble() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	bubble();
+
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", arr[i]);
+	}
 }
